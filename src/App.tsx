bd0a1164303ceaf323716e6c7758473192048768
Henry Jordan <henry.jordan@credera.com>
2022-06-14 21:50:31 -0500

    CREATE | initial scaffolding for hackathon

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav className="app-nav">
+        <Link to="/">Home</Link>
+        <Link to="objective">Objective</Link>
+        <Link to="teams">Teams</Link>
+        <Link to="thanks">Thank You</Link>
+      </nav>
+      <div className="app-content">
+        <Outlet />
+      </div>
+      <footer>
+        <p>Credera ♥️ XD</p>
+      </footer>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import ObjectivePage from "./pages/Objective";
+import HomePage from "./pages/Home";
+import TeamsPage from "./pages/Teams";
+import ThanksPage from "./pages/Thanks";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="objective" element={<ObjectivePage />} />
+          <Route path="teams" element={<TeamsPage />} />
+          <Route path="thanks" element={<ThanksPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
